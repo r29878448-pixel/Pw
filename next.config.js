2c871e80d18ad4b92940dd64df00b5ee2d1a9c55
adityaghoghari77-maker <adityaghoghari77@gmail.com>
2026-03-30 16:33:01 +0530

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: { domains: ['static.pw.live', 'd2bps9p1kiy4ka.cloudfront.net', 'i.postimg.cc'] },
+}
